f0f52e55ba044999767971fcbfdfa1fda76ef2ae
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-04 16:42:33 +0530

    Removing comments.

--- Program.cs
+++ Program.cs
@@ -2,16 +2,17 @@
 {
     class Program
     {
+            
         public static void Main(string[] args)
         {
-            //fibonasi.CalculateFibonaciiSeries();
-            //PerfectNumber.CalculatePerfectNumber();
-            //ReverseNumber.Reverse();
-            //CuponNumbers.DinstinctCuponNumbers();
+            fibonasi.CalculateFibonaciiSeries();
+            PerfectNumber.CalculatePerfectNumber();
+            ReverseNumber.Reverse();
+            CuponNumbers.DinstinctCuponNumbers();
             PrimeNumber.CheckPrimeNumber();
 
-            //StopWatch stopwatch = new StopWatch();
-            //stopwatch.CollapseTime();
+            StopWatch stopwatch = new StopWatch();
+            stopwatch.CollapseTime();
         }
     }
 }
