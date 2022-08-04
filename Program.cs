fd85ddc2b653b757ee40eaacc06b28795822244a
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-04 15:56:42 +0530

    Program file for programs added.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LogicalPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //fibonasi.CalculateFibonaciiSeries();
+            //PerfectNumber.CalculatePerfectNumber();
+            //ReverseNumber.Reverse();
+            //CuponNumbers.DinstinctCuponNumbers();
+            PrimeNumber.CheckPrimeNumber();
+
+            //StopWatch stopwatch = new StopWatch();
+            //stopwatch.CollapseTime();
+        }
+    }
+}
