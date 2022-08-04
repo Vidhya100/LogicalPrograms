ea7263fa2cba20fe72de16bf643fa3327bd4dc46
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-04 16:18:23 +0530

    Finding elapsed time in stopwatch

---
+++ StopWatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class StopWatch
+    {
+        public void CollapseTime()
+        {
+            //Created object for StopWatch function which is inbuilt in System.Diagnostics
+            Stopwatch stopWatch = new Stopwatch();            
+
+            Console.Write("Enter any key to start stopwatch\n ");
+            Console.ReadKey();
+
+            //started stopwatch
+            stopWatch.Start();
+
+            Console.WriteLine("\nEnter any key to stop stopwatch");
+            Console.ReadKey();
+                        
+            //stop stopWatch
+            stopWatch.Stop();
+
+            //It will give us elapsed time
+            TimeSpan timeSpan = stopWatch.Elapsed;
+            
+            Console.WriteLine("\nTime elapsed : {0:hh\\:mm\\:ss}", stopWatch.Elapsed);
+        }
+       
+    }
+}
